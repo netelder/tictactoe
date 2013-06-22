1104d1223f36b4e24b719e0cf6d5b9ed0d0d425c
DBC-Apprentice <dbc@dbc.com>
2013-06-21 17:09:58 -0700

    ajax request game, jquery to create a url

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,8 +1,17 @@
-get '/game/start' do
+get '/game/start/' do
 #empty game board
   @user_id = currentuser.id
   erb :play
 end
+
+post '/game/start/:user_id' do
+#empty game board
+@user = User.find()
+@game = Game.create
+#pass the user into game user_1
+{username:  @user.username, game_id: @game.id}
+end
+
 
 get 'game/:game_id/user/:user_id' do
   # ajax route for getting status (my turn?)
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+$('#new_game').on('submit', function(e){
+  e.preventDefault();
+  $.ajax({
+    Type:  "POST",
+    Url:  "/game/start/" + '<%= user.id %>',
+    Data:  "json"
+  }).done(function (response){
+  console.log(response);
+$("#waiting_players li").append('<li><a href="game/' + response.game_id + response.username + '>response.username</a></li>');
+    });
+  )};
+});
+
 
   $('#board').on('click', 'td', function (e) {
     var cell = e.target;
@@ -11,6 +24,6 @@ $(document).ready(function() {
     }
   });
 
-  
+
 
 });
